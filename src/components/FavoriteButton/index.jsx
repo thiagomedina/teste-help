445510ceb15006bcc4fb20eb2745f11b7f221d31
thiagomedina <thiagomedina001@gmail.com>
2021-01-02 10:12:49 -0300

    create a search component

--- src/components/FavoriteButton/index.jsx
+++ src/components/FavoriteButton/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {FiStar} from 'react-icons/fi';
 
 
 const FavoriteButton = ({track}) => {
-
+return(
+<FiStar/>
+)
 }
 
 export default FavoriteButton
---
+++ src/container/Search/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SearchInput from '../../components/SearchInput';
+
+const Search = () => {
+  return <SearchInput />;
+};
+
+export default Search;
